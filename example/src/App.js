6f5797cbeaf11c3a5925611d83de86872b089ece
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-04-27 11:32:02 +0200

    Update dependencies

--- example/src/App.js
+++ example/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import Timer from 'react-timer-component'
+import Timer from 'simple-react-timer'
 
 class App extends Component {
   render () {
